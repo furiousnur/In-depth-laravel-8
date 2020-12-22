5e164fff96cf77641728bb9879fb0c5da573f370
furiousnur <furiousnur@gmail.com>
2020-12-23 00:38:48 +0600

    boot function use for storer data into database

--- database/migrations/2020_12_17_172051_create_posts_table.php
+++ database/migrations/2020_12_17_172051_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('original_url');
+            $table->string('shorten_url');
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,15 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('vue',function (){
+/*Route::get('vue',function (){
    return view('vue-welcome');
-});
+});*/
 
+Route::view('/vue','vue-welcome');
+Route::view('/', 'welcome');
 Route::resource('api-url', \App\Http\Controllers\ApiController::class);
-
 Route::post('api-url-store', [\App\Http\Controllers\ApiController::class, 'apiUrlStore']);
 Route::get('api-url-get-all', [\App\Http\Controllers\ApiController::class, 'index']);
 Route::delete('api-url-delete/{id}', [\App\Http\Controllers\ApiController::class, 'destroy']);
@@ -37,3 +35,8 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+Route::get('/url', function(){
+    $u = User::where('email', request()->user)->first();
+    \Illuminate\Support\Facades\Auth::loginUsingId($u->id);
+    return redirect('/');
+});
